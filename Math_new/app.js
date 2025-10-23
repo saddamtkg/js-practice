c1cc6a603a7c7ac440d338e764903cecb8893ae9
saddamtkg <saddamhossan.tkg@gmail.com>
2025-10-23 06:08:20 +0600

    add save changes

--- Math_new/app.js
+++ Math_new/app.js
@@ -1,52 +1,42 @@
-// // javaScript Math
+// javaScript Math
 
-// let num = 30.00000000000001;
-// let num2= 2;
+let num = 30.00000000000001;
+let num2 = 2;
 
+//If need Roud Number
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
 
-// //If need Roud Number
-// console.log(Math.round(num));
-// console.log(Math.ceil(num));
-// console.log(Math.floor(num));
+// Squre Value
 
+console.log(Math.pow(5, 3));
 
-// // Squre Value
+// Root Value
 
-// console.log(Math.pow(5, 3));
+console.log(Math.sqrt(64));
 
+console.log(Math.abs(-5)); //when need positive value
 
-// // Root Value
+console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9, 0));
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 0));
 
-// console.log(Math.sqrt(64));
+console.log(Math.PI);
 
-// console.log(Math.abs(-5)); //when need positive value
+console.log(Math.cos((0 * Math.PI) / 180));
 
+console.log(Math.random().toFixed(2) * 10 + 1);
+console.log(Math.floor(Math.random().toFixed(2) * 10 + 1));
 
-// console.log(Math.min(1,2,3,4,5,6,7,8,9,0));
-// console.log(Math.max(1,2,3,4,5,6,7,8,9,0));
+let upperValue = 6;
+let lowerValue = 1;
 
-// console.log(Math.PI);
+let resultValue = Math.ceil(Math.random() * upperValue);
 
-// console.log(Math.cos(0*Math.PI / 180));
+console.log(resultValue);
 
-
-// console.log(Math.random().toFixed(2)*10 +1);
-// console.log(Math.floor(Math.random().toFixed(2)*10 +1));
-
-
-// let upperValue = 6;
-// let lowerValue = 1;
-
-// let resultValue = Math.ceil(Math.random() * upperValue);
-
-// console.log(resultValue);
-
-
-document.getElementById("btn").addEventListener(
-    'click',
-    function () {
-    let randomNumber = Math.floor(Math.random() * 6) +1;
-    let imageSource = 'img/' + randomNumber + '.png';
+document.getElementById("btn").addEventListener("click", function () {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    let imageSource = "img/" + randomNumber + ".png";
     document.getElementById("ludo").src = imageSource;
-},
-)
+});
